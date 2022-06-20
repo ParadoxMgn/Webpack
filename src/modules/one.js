56a9a12e402401eaeb5cd35e2415b7a727420d6c
Eugene Shumkov <radoxmgn@gmail.com>
2022-06-20 21:11:24 +0300

    first commit

---
+++ src/modules/one.js
@@ -0,0 +1,5 @@
+const showFileName = () => {
+  console.log('one.js');
+};
+
+export default showFileName;
---
+++ src/modules/two.js
@@ -0,0 +1,5 @@
+const showFileName = () => {
+  console.log('two.js');
+};
+
+export default showFileName;
